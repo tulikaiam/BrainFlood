ce1e605f7e9d7738045246e2559df0a08c444357
Tulika Sureka <tulikasureka4@gmail.com>
2018-10-14 00:21:44 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,27 @@
+<?php
+$host = 'codefundo.mysql.database.azure.com';
+$username = 'codefundo@codefundo';
+$password = 'Microsoft123';
+$db_name = 'mysql';
+
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+//Create an Insert prepared statement and run it
+$product_name = 'BrandNewProduct';
+$product_color = 'Blue';
+$product_price = 15.5;
+if ($stmt = mysqli_prepare($conn, "INSERT INTO Products (ProductName, Color, Price) VALUES (?, ?, ?)")) {
+mysqli_stmt_bind_param($stmt, 'ssd', $product_name, $product_color, $product_price);
+mysqli_stmt_execute($stmt);
+printf("Insert: Affected %d rows\n", mysqli_stmt_affected_rows($stmt));
+mysqli_stmt_close($stmt);
+}
+
+// Close the connection
+mysqli_close($conn);
+?>
