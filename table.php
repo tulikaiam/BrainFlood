a62de1f7be9592ff3ab3df28eaae755a3f314cbf
Tulika Sureka <tulikasureka4@gmail.com>
2018-10-14 00:07:00 +0530

    Update table.php

--- table.php
+++ table.php
@@ -2,7 +2,7 @@
 $host = 'codefundo.mysql.database.azure.com';
 $username = 'codefundo@codefundo';
 $password = 'Microsoft123';
-$db_name = 'codefundo';
+$db_name = 'mysql';
 
 //Establishes the connection
 $conn = mysqli_init();
