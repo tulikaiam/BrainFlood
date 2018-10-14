f849b27f1cc1818219624722e502f15ad118c32a
Tulika Sureka <tulikasureka4@gmail.com>
2018-10-14 22:35:40 +0530

    Update new.php

--- new.php
+++ new.php
@@ -1,21 +1,4 @@
-<?php
 
-header("Access-Control-Allow-Origin:*");
-$host = 'codefundo.mysql.database.azure.com';
-$username = 'codefundo@codefundo';
-$password = 'Microsoft123';
-$db_name = 'users';
-
-$conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
-$data = mysqli_query($conn, "SELECT * FROM Maps");
-    foreach ($data as $key)
-    {
-                $locations[]=array( 'name'=>$key['Name'], 'lat'=>$key['Lat'], 'lng'=>$key['Lon'] );
-    }
-    /* Convert data to json */
-    $markers = json_encode( $locations );
-    ?>
 
 <html>
   <head>
@@ -44,7 +27,24 @@ $data = mysqli_query($conn, "SELECT * FROM Maps");
           zoom: 10,
           center: {lat: 12.1769, lng: 77.1237 }
         });
-        
+        <?php
+
+header("Access-Control-Allow-Origin:*");
+$host = 'codefundo.mysql.database.azure.com';
+$username = 'codefundo@codefundo';
+$password = 'Microsoft123';
+$db_name = 'users';
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+$data = mysqli_query($conn, "SELECT * FROM Maps");
+    foreach ($data as $key)
+    {
+                $locations[]=array( 'name'=>$key['Name'], 'lat'=>$key['Lat'], 'lng'=>$key['Lon'] );
+    }
+    /* Convert data to json */
+    $markers = json_encode( $locations );
+    ?>
       var json=JSON.parse( markers );
 
             for( var o in json ){
