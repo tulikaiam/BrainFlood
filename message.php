b15c0e0a68c9e02065e1417a0d32d2bb32b39e80
Tulika Sureka <tulikasureka4@gmail.com>
2018-10-19 19:52:28 +0530

    Update message.php

--- message.php
+++ message.php
@@ -2,10 +2,10 @@
 header("Access-Control-Allow-Origin:*");
 extract($_GET);
 //echo $locations;
-$point = explode(',', $data);
+$point = explode('_', $data);
 $msg =  $point[0];
 $no = $point[1];
 //Create an Insert prepared statement and run it
-$res = json_decode(file_get_contents('https://smsapi.engineeringtgr.com/send/?Mobile=9663126670&Password=tulika&Message='.$msg.'&To='.$no.'&Key=tulikPEUuMjSBrXdnTGbW81eK'),true);
+$res = json_decode(file_get_contents('http://api.msg91.com/api/sendhttp.php?country=91&sender=MSGIND&route=4&mobiles='.$no.'&authkey=243560AIvvhnmj0C5bc9e6ba&message='.$msg),true);
 
 echo $res;
